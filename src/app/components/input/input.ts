3eeb039152189d2bd181d079904ee7e9f96ac3ee
katakicodes <katakicode001@gmail.com>
2025-07-25 21:47:21 +0100

    feat: Create white bg input

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input as input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.html',
   styleUrl: './input.scss'
 })
 export class Input {
+  @input() classname:string;
   @input() type:string = '';
   @input() placeholder:string = '';
 }
