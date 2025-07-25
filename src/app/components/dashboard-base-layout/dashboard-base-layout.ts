551e1cebb6bdc1bb4bbb1404fafbfa46ec828d15
katakicodes <katakicode001@gmail.com>
2025-07-25 21:46:14 +0100

    feat: Show date dinamically in dashboard

--- src/app/components/dashboard-base-layout/dashboard-base-layout.ts
+++ src/app/components/dashboard-base-layout/dashboard-base-layout.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DateStringValues } from '../../shared/DateStringValues';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-base-layout',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-base-layout.scss'
 })
 export class DashboardBaseLayout {
+  protected weekday:string;
+  protected date:Date;
+  @Input() sectionPage:string;
+  protected user:User;
+  private datestringValues:DateStringValues
+  
+  constructor(){
+    this.date = new Date();
+    this.user = {id:1, email:"nelson@gmail.com", name:"Nelson Dos Santos"}
+    this.datestringValues = new DateStringValues();
+  }
 
+  public getWeekDay():string{
+    return this.datestringValues.weekdays[this.date.getDay()];
+  }
+  public getMonth():string{
+    return this.datestringValues.months[this.date.getMonth()]
+  }
 }
