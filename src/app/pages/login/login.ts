80e64fdc12362b34de9a928ac90887bae7040595
katakicodes <katakicode001@gmail.com>
2025-07-27 10:49:47 +0100

    fix: remove input component

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { BaseLogin } from "../../components/base-login/base-login";
 import { Button } from "../../components/button/button";
-import { Input } from "../../components/input/input";
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [BaseLogin, Button, Input, RouterLink],
+  imports: [BaseLogin, Button, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
