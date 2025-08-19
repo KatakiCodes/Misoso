63c1354eb605b7d3a12eb3f9b89780d8813bd722
katakicodes <katakicode001@gmail.com>
2025-08-19 17:31:33 +0100

    feat: Home page created

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { BaseLayout } from "../../components/base-layout/base-layout";
 import { DashboardCard } from "../../components/dashboard-card/dashboard-card";
+import { TaskModel } from '../../Models/TaskModel';
+import { TaskItemCard } from "../../components/task-item-card/task-item-card";
+import { Title } from '@angular/platform-browser';
+import { SubtaskModel } from '../../Models/SubtaskModel';
 
 @Component({
   selector: 'app-home',
-  imports: [BaseLayout, DashboardCard],
+  imports: [BaseLayout,DashboardCard,TaskItemCard],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
-
+export class Home{
+  @Input() taskFocused:TaskModel;
+  constructor(){
+    this.taskFocused = {
+    Id: 1,
+    UserId: 1,
+    Title: 'Complet the dashboard',
+    Description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae iusto sapiente odit, iste ex dolores. Saepe iste dolores tempora deleniti nam. Asperiores et corrupti ullam minima natus in, placeat soluta?',
+    CreatedAt: new Date(),
+    UpdatedAt:new Date(),
+    ToFinishAt:new Date(),
+    FinishedAt:new Date(),
+    IsFocused:true,
+    IsLate:false,
+    IsConcluded:false,
+    subtasks:[] = [
+      {
+        Id:1,
+        TaskId:1,
+        Title:'Create task section for show more details',
+        CreatedAt:new Date(),
+        UpdatedAt:new Date(),
+        IsFocused:true,
+        IsConcluded:false
+      },
+      {
+        Id:2,
+        TaskId:1,
+        Title:'Build the subtask section',
+        CreatedAt:new Date(),
+        UpdatedAt:new Date(),
+        IsFocused:false,
+        IsConcluded:false
+      },
+      {
+        Id:3,
+        TaskId:1,
+        Title:'Finishing the screen',
+        CreatedAt:new Date(),
+        UpdatedAt:new Date(),
+        IsFocused:false,
+        IsConcluded:false
+      }
+    ] 
+    }
+  }
 }
