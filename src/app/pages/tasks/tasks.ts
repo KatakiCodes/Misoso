ba625810e522823a6808ef966991e0fceca6209c
katakicodes <katakicode001@gmail.com>
2025-08-22 11:48:11 +0100

    feat: Task list screen created

---
+++ src/app/pages/tasks/tasks.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BaseLayout } from "../../components/base-layout/base-layout";
+
+@Component({
+  selector: 'app-tasks',
+  imports: [BaseLayout],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.scss'
+})
+export class Tasks {
+
+}
