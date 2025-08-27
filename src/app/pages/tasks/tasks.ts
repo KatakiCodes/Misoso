a6ca77c9cad2ec2f546f6b4aba549b4cbd45044e
katakicodes <katakicode001@gmail.com>
2025-08-27 11:47:22 +0100

    feat: Create the form to (create) task

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { BaseLayout } from "../../components/base-layout/base-layout";
+import { TaskForm } from "../../components/task-form/task-form";
 
 @Component({
   selector: 'app-tasks',
-  imports: [BaseLayout],
+  imports: [BaseLayout, TaskForm],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss'
 })
 export class Tasks {
+  showTaskForm:boolean = false;
+  createTaskButtonText:string = '+ New task';
 
+
+  toggleCreateTaskButtonText(){
+    this.showTaskForm = (this.showTaskForm) ? false:true;
+    this.createTaskButtonText = (!this.showTaskForm) ?'+ New task':'Cancel';
+    console.log(this.createTaskButtonText);
+  }
 }
