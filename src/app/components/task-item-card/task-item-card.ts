daa24ba6c8f75f8e5dbf5977282b14d084abb58c
katakicodes <katakicode001@gmail.com>
2025-09-08 21:31:18 +0100

    create-form-to-create-subtasks

--- src/app/components/task-item-card/task-item-card.ts
+++ src/app/components/task-item-card/task-item-card.ts
@@ -2,14 +2,25 @@ import { Component, Input } from '@angular/core';
 import { TaskModel } from '../../Models/TaskModel';
 import { CommonModule } from '@angular/common';
 import { TaskForm } from "../task-form/task-form";
+import { SubtaskForm } from "../subtask-form/subtask-form";
 
 @Component({
   selector: 'app-task-item-card',
-  imports: [CommonModule, TaskForm],
+  imports: [CommonModule, TaskForm, SubtaskForm],
   templateUrl: './task-item-card.html',
   styleUrl: './task-item-card.scss'
 })
 export class TaskItemCard {
   @Input() task:TaskModel;
-  @Input() showSubtaskActions: boolean = false;
+  @Input() showSubtaskActions: boolean;
+  subtaskForm: boolean;
+
+  constructor(){
+    this.showSubtaskActions = false;
+    this.subtaskForm = false;
+  }
+
+  toggleSubtaskForm(){
+    this.subtaskForm = (this.subtaskForm) ? false : true;
+  }
 }
